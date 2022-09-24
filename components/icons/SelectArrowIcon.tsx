0a5358a7e3a22753ebb6d2dd14512fc490134204
Jason Haala <jason@haalabrands.com>
2022-09-24 18:28:30 -0400

    Adds icon and pagination components

---
+++ components/icons/SelectArrowIcon.tsx
@@ -0,0 +1,8 @@
+const SelectArrowIcon = (): JSX.Element => {
+	return (
+		<span className="absolute right-4 top-1/2 mt-[-2px] h-[10px] w-[10px] -translate-y-1/2 rotate-45 border-r-2 border-b-2 border-body-color">
+		</span>
+	);
+};
+
+export default SelectArrowIcon;
